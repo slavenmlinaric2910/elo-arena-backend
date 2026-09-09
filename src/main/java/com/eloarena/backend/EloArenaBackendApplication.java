9446334569c34a083a268dd295b82d707f986d9b
Slaven Mlinaric <slavenmlinaric2910@gmail.com>
2026-09-09 20:36:50 +0200

    Initialize Spring Boot backend

---
+++ src/main/java/com/eloarena/backend/EloArenaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eloarena.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EloArenaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EloArenaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eloarena/backend/EloArenaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eloarena.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EloArenaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
